50603a1518d1d3cf2d8f378612604858f05986bf
CHRoosipuu <carlhenry@roosipuu.com>
2019-09-23 13:45:28 -0400

    Working

--- src/poem/cw.js
+++ src/poem/cw.js
@@ -1,115 +1,91 @@
 window.onload = function makeList() {
-    // Establish the array which acts as a data source for the list
-    let listData = wordObjs[5],
-    // Make a container element for the list
-    listContainer = document.createElement('div'),
     
-    // Make the list
-    listElement = document.createElement('ul'),
+    // Establish the array which acts as a data source for the list
+    let listData = wordObjs[5];
+    
+    // Make a container element for the lists and set HTML class tag
+    let listContainer = document.createElement('div');
+    listContainer.className = "wordLists";
+    
+    // Create HTML list elements for mastered and unmastered words and set HTML class tag
+    // Mastered words
+    let masteredWordsListElement = document.createElement('ul');
+    masteredWordsListElement.className = "masteredWordsListElement wordsListElement";
+    // Unmastered words
+    let unmasteredWordsListElement = document.createElement('ul');
+    unmasteredWordsListElement.className = "unmasteredWordsListElement wordsListElement";
+
+    // Add lists to the list container
+    listContainer.append(masteredWordsListElement)
+    listContainer.append(unmasteredWordsListElement)
+    
     // Set up a loop that goes through the items in listItems one at a time
-    numberOfListItems = listData.length,
-    listItem,
-    i;
+    let numberOfListItems = i;
+    
+    // Create a list item for each word and place in apropriate list
+    for (i = 0; i < listData.length; ++i) {
 
-    // Establish the array which acts as a data source for the list
-    let listElement2 = document.createElement('ul');
-    
-    listElement2.className = "listElement2 listcol";
-    listElement.className = "listElement listcol";
-    
-    listContainer.className = "wordList";
-    
-    
-//    console.log(listData);
-    // Add it to the page
-
-    
-//    listElement.appendChild(listItem);
-
-    for (i = 0; i < numberOfListItems; ++i) {
-        // create an item for each one
-        listItemLink = document.createElement('a');
-        listItem = document.createElement('li');
-        listItem.addEventListener('click', function(){
-        console.log("Child clicked");
-      });
+        // Create the HTML list item and set HTML class tag
+        let listItem = document.createElement('li');
         listItem.className = "WordItem clickable";
         
-        const clip_name = '../../assets/word_assets/word_audio/' + listData[i] + '.mp3'
+        // Get the name of the word
+        const wordName = listData[i].word;
         
-        
+        // Set up word audio on mouse over
+        const clip_name = '../../assets/word_assets/word_audio/' + wordName + '.mp3';    
         listItem.onmouseover = function(){playClip(clip_name);};
         listItem.onmouseout = function(){stopClip(clip_name);};
         
-        
+        // Log word name when list item is clicked
+        listItem.onclick = function(){console.log("Clicked="+wordName);};
+
+        // Add the word name to the list item
+        listItem.innerHTML = '<h2>' + wordName + '</h2>';
+        // Add the word image to the list item
         imageItem = document.createElement('img');
-        
-
-
-        // Add the item text
-        imageItem.src = '../../assets/word_assets/word_art/5/' + listData[i].word + '.png';
-        listItem.innerHTML = '<h2>' + listData[i].word + '</h2>';
-            imageItem.onclick = function(){console.log("0");}
-        
+        imageItem.src = '../../assets/word_assets/word_art/5/' + wordName + '.png';
         listItem.appendChild(imageItem);
         
+        
+        // Onclick function for all list items
+        listItem.onclick = function(){console.log(wordName);};
         
         
         // Add listItem to the listElement
         if (listData[i].learned) {
-            
-            listElement.innerHTML += listItem.outerHTML;
+            // Mastered words
+            masteredWordsListElement.append(listItem);
         } else {
-            
-            listItemLink.href = "../quiz/quiz.html";
-            listItemLink.innerHTML += listItem.outerHTML;
-            listElement2.innerHTML += listItemLink.outerHTML;
+            // Unmastered words
+            // Set onclock to go to quiz page
+            listItem.onclick = function(){window.location.href = '../quiz/quiz.html';};
+            unmasteredWordsListElement.append(listItem);
         }
     }
-    document.getElementsByTagName('body')[0].appendChild(listContainer);
-//    listContainer.appendChild(listElement);
-//    listContainer.appendChild(listElement2);
     
-    listContainer.innerHTML += listElement.outerHTML + listElement2.outerHTML;
+    // Add the lists div to the body of page
+    document.getElementsByTagName('body')[0].appendChild(listContainer);
 }
 
 
 function playClip(clip_name) {
-    if (playing) {return;}
-  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
-    if (document.all) {
-      document.all.sound.src = "click.mp3";
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+        if (document.all) {
+            document.all.sound.src = "click.mp3";
+        }
+    } else {
+        var audio = document.getElementById("word_audio");
+        audio.src = clip_name;
+        const playPromise = audio.play();
+        if (playPromise !== null){
+            playPromise.catch(() => { console.log("Caught: playPromise !== null"); })
+        }
     }
-  }
-  else {
-    {
-    var audio = document.getElementById("word_audio");
-    
-    
-        
-    if (audio == null ) {
-        return;
-    }
-    
-    audio.src = clip_name;
-    const playPromise = audio.play();
-    if (playPromise !== null){
-      playPromise.catch(() => { console.log("Caugth"); })
-      console.log("User needs to click on page first");
-    }
-    }
-  }
-    playing = true;
-    console.log("playing=" + playing);
 }
 
 function stopClip(clip_name) {
-  var audio = document.getElementById("word_audio");
-    if (audio == null) {
-        return;
-    }
-  audio.pause();
-  audio.currentTime = 0;
-    playing = false;
-    console.log("playing=" + playing);
+    var audio = document.getElementById("word_audio");
+    audio.pause();
+    audio.currentTime = 0;
 }
