9046a7ad91cc4db709970711729b822fc5da891d
Anna Phillips <annalouisa.phillips@gmail.com>
2019-09-23 10:58:25 -0400

    List of buttons on words page

--- src/wordbank/wordbank.js
+++ src/wordbank/wordbank.js
@@ -1,9 +1,40 @@
-function makeButtons() {
-    for (var i = 0; i < categories.length; i++) {
-        var button = document.createElement("button");
-        var t = document.createTextNode(categories[i]);
-        button.appendChild(t);
-        document.body.appendChild(button);
+window.onload = function makeList() {
+    // Establish the array which acts as a data source for the list
+    let listData = words[1],
+    // Make a container element for the list
+    listContainer = document.createElement('div'),
+    
+    // Make the list
+    listElement = document.createElement('ul'),
+    // Set up a loop that goes through the items in listItems one at a time
+    numberOfListItems = listData.length,
+    listItem,
+    i;
+    
+    listContainer.className = "wordList";
+    
+    
+//    console.log(listData);
+    // Add it to the page
+    document.getElementsByTagName('body')[0].appendChild(listContainer);
+    listContainer.appendChild(listElement);
+
+    for (i = 0; i < numberOfListItems; ++i) {
+        // create an item for each one
+        listItem = document.createElement('button');
+        listItem.className = "WordItem clickable";
+        
+        
+        imageItem = document.createElement('img');
+
+        // Add the item text
+        imageItem.src = '../../assets/word_assets/word_art/1/' + listData[i] + '.png';
+        listItem.innerHTML = '';
+        listItem.innerHTML = '<h2>' + listData[i] + '</h2>';
+        listItem.appendChild(imageItem);
+        
+
+        // Add listItem to the listElement
+        listElement.appendChild(listItem);
     }
 }
-makeButtons();
