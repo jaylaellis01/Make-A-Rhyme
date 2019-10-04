10df83cd570d8f699e3883228ee0118ccbac468b
Anna Phillips <annalouisa.phillips@gmail.com>
2019-10-03 21:26:04 -0400

    Added comments to wordbank html/css/js

--- src/wordbank/categories.js
+++ src/wordbank/categories.js
@@ -1,19 +1,23 @@
+// Creates category buttons 
 window.onload = function makeButtons() {
     let categoryList = categories;
     console.log(categoryList.length);
     numberOfItems = categoryList.length,
     i;
+    //Setup div to hold the buttons
     listContainer = document.createElement('div');
 
     listContainer.className = "tab";
     document.getElementsByTagName('body')[0].appendChild(listContainer);
 
     for(i = 1; i <= 19; i++) {
-        console.log("test");
+        // create a button for each category in the list
         listItem = document.createElement('button');
         listItem.className = "tablinks clickable";
         listItem.id = i;
+        //onclick function used to populate words page
         listItem.onclick = function(){categoryClick(this.id)};
+        //create image element for each button
         imageItem = document.createElement('img');
         imageItem.src = '../../assets/category_assets/' + categoryList[i] + '.png';
         listItem.innerHTML = categoryList[i];
