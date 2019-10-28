efcf308d25b6342bae7069cf49694b76844c9f0a
CHRoosipuu <carl@chroosipuu.com>
2019-10-28 12:33:06 -0400

    Messy poem writing and audio

--- src/poem/poem.js
+++ src/poem/poem.js
@@ -444,29 +444,72 @@ function stopClip(clip_name) {
 function playNextAudio(poem, index) {
     let audioPath = "../../assets/audio/" + poem + "/" + index + ".mp3";
     playPoemClip(audioPath);
-    
-    // console.log(audioPath);
 }
 
-function readPoem() {
+// type one text in the typwriter
+// keeps calling itself until the text is finished
+function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+        // add next character to h1
+        document.getElementById("poem_text").innerHTML = text.substring(0, i+1);
+
+        // wait for a while and call this function again for next character
+        setTimeout(function() {
+            typeWriter(text, i + 1, fnCallback)
+        }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+        // call callback after timeout
+        setTimeout(fnCallback, 700);
+    }
+}
+// start a typewriter animation for a text in the dataText array
+function StartTextAnimation(i, txt) {
+    if (typeof txt == 'undefined'){
+        setTimeout(function() {
+            StartTextAnimation(0);
+        }, 20000);
+    }
+    // check if dataText[i] exists
+    if (i < txt.length) {
+        // text exists! start typewriter animation
+        typeWriter(txt, 0, function(){
+            // after callback (and whole text has been animated), start next text
+            StartTextAnimation(i + 1);
+        });
+    }
+}
+
+
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 300; /* The speed/duration of the effect in milliseconds */
+
+function typeNextText(txt) {
+    document.getElementById("poem_text").innerHTML = "";
+    i = 0;
+    StartTextAnimation(i, txt);
+}
+
+function readPoem(textArr) {
+    console.log(textArr);
     var poem = getCookie("currentPoem");
     var poemIndex = 1;
     for (var i = 0; i < textArr.length; i++) {
         playNextAudio(poem, i+1);
-        displayText(i);
-        chooseWord(boxArr[i], canvasState);
+        var text = textArr[i];
+        console.log(text);
+        typeNextText(text);
 
     }
 
 }
 function chooseWord(boxID, canvasState) {
     var word = canvasState.selectWordWithID(boxID);
-    
 }
-$.getJSON("poem_data.json", function(data) {
-    var textArr = data["poems"][getCookie("currentPoem")]["text"];
-    var boxArr = data["poems"][getCookie("currentPoem")]["cueBox"];
-});
+
 // Init function called on page load
 function init() {
     // console.log(getCookie("currentPoem"));
@@ -489,8 +532,12 @@ function init() {
                 s.addWord(new WordBox(word["x"], word["y"], 1, 1, fillColor, word["categories"], word["spot-id"]))
             } 
         });
-        
     }
+    $.getJSON("poem_data.json", function(data) {
+        var boxArr = data["poems"][getCookie("currentPoem")]["cueBox"];
+        readPoem(data["poems"][getCookie("currentPoem")]["text"]);
+        console.log("end get json");
+    });
 }
 
 
